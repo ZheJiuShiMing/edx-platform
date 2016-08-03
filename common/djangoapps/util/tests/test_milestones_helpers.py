2cf0b9bae8dbc5a76aab78a52f42ebde038e5de2
Sanford Student <sanfordstudent@C02QJ0RCG8WM.tld>
2016-08-03 13:23:55 -0400

    fixing some tests

--- common/djangoapps/util/tests/test_milestones_helpers.py
+++ common/djangoapps/util/tests/test_milestones_helpers.py
@@ -115,8 +115,12 @@ class MilestonesHelpersTestCase(ModuleStoreTestCase):
         response = milestones_helpers.get_service()
         self.assertIsNone(response)
 
+    @patch.dict('django.conf.settings.FEATURES', {'MILESTONES_APP': True})
     def test_any_unfulfilled_milestones(self):
-        """ Tests any_unfulfilled_milestones for invalid arguments """
+        """
+        Tests any_unfulfilled_milestones for invalid arguments with
+        the app enabled
+         """
         with self.assertRaises(InvalidCourseKeyException):
             milestones_helpers.any_unfulfilled_milestones(None, self.user)
         with self.assertRaises(InvalidUserException):
